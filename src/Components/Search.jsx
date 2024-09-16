c1f6a38f63c2e6e92ed997b8f12158085aa3320d
mehtapiyush530 <mehtapiyush530@gmail.com>
2024-09-16 16:38:20 +0530

    changed

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -6,7 +6,7 @@ const Search = ({ searchQuery, setSearchQuery }) => {
       <input
         type="text"
         placeholder="Enter country name:"
-        className="h-[3rem] w-[20rem] border border-black ml-[30px] mt-[50px]"
+        className="h-[3rem] w-[20rem] border border-black ml-[30px] mt-[50px] px-4"
         value={searchQuery}
         onChange={(e) => setSearchQuery(e.target.value)}
       />
